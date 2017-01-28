c6999e2fc055404dd9c3d401ac4a2b3de19e2f32
maciejsobotka <sobot92@tlen.pl>
2017-01-28 18:58:06 +0100

    Added Jewels to collect.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
     #region Private fields
 
+    private int m_JewelsCount;
+
     private float m_JumpSpeed;
     private Rigidbody m_Rigidbody;
+    private Text m_ScoreText;
     private float m_Speed;
 
     #endregion
@@ -23,12 +27,30 @@ public class BallController : MonoBehaviour
         m_Rigidbody.AddForce(movement);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Jewel"))
+        {
+            other.gameObject.SetActive(false);
+            m_JewelsCount++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        m_ScoreText.text = "Score: " + m_JewelsCount;
+    }
+
     // Use this for initialization
     private void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
         m_Speed = 15;
         m_JumpSpeed = 20;
+        m_JewelsCount = 0;
+        m_ScoreText = GetComponentInParent<Canvas>().GetComponent<Text>();
+        SetCountText();
     }
 
     #endregion
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    #region Private methods
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    #endregion
+}
