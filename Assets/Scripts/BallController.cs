0c12ccf4e7289e4040c7d4e5352c9fd4fd8da069
maciejsobotka <sobot92@tlen.pl>
2017-01-28 14:27:42 +0100

    Initial Commit.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    #region Private fields
+
+    private float m_JumpSpeed;
+    private Rigidbody m_Rigidbody;
+    private float m_Speed;
+
+    #endregion
+    #region Private methods
+
+    // All rigidbody manipulated should be performed in FixedUpdate
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveJump = Input.GetAxis("Jump");
+
+        var movement = new Vector3(moveHorizontal * m_Speed, moveJump * m_JumpSpeed, moveVertical * m_Speed);
+
+        m_Rigidbody.AddForce(movement);
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Speed = 15;
+        m_JumpSpeed = 20;
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region Private fields
+
+    private GameObject m_Ball;
+
+    private Vector3 m_Offset;
+
+    #endregion
+    #region Private methods
+
+    // Runs after fame
+    private void LateUpdate()
+    {
+        transform.position = m_Ball.transform.position + m_Offset;
+    }
+
+    private void Start()
+    {
+        m_Ball = GameObject.Find("Ball");
+        m_Offset = transform.position - m_Ball.transform.position;
+    }
+
+    #endregion
+}
